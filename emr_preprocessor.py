1be0d95fdc7b2679d6c46f1b9d7c6edd924282e5
Brian-Ckwu <sonny40920710@gmail.com>
2022-07-07 16:01:42 +0800

    move function build_patient_states() from finding_extractor.py to emr_preprocessor.py

--- emr_preprocessor.py
+++ emr_preprocessor.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union, Dict, Tuple
 
 from finding_extractor import FindingExtractor
 
@@ -28,4 +28,23 @@ class EMRPreprocessor(object):
             text_l.append(text)
         
         return text_l
+
+    @staticmethod
+    def build_patient_states(terms_l: List[List[str]], pols_l: List[List[int]], return_type: bool = "dict") -> Union[List[Dict[str, int]], List[List[Tuple[str, int]]]]:
+        assert len(terms_l) == len(pols_l)
+        if return_type not in ["dict", "tuple"]:
+            raise ValueError("return_type should be 'dict' or 'tuple'")
+
+        states = list()
+        for terms, pols in zip(terms_l, pols_l):
+            assert len(terms) == len(pols)
+            state = dict() if return_type == "dict" else list()
+            for term, pol in zip(terms, pols):
+                t = (term, pol)
+                if return_type == "dict":
+                    state[t] = state.get(t, 0) + 1
+                elif return_type == "tuple":
+                    state.append(t)
+            states.append(state)
         
+        return states
