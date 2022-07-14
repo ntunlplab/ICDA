d82cfcc9d9bf248ddf84b05116177e80513a11b4
Brian-Ckwu <sonny40920710@gmail.com>
2022-07-14 11:24:36 +0800

    implement 3 functions in DiagnosisClassifer

--- diagnosis_classifier.py
+++ diagnosis_classifier.py
@@ -1,10 +1,60 @@
-from typing import List
+from shutil import move
+from tqdm import tqdm
+from typing import List, Dict, Any, Tuple
 
-from model import BertDxModel
+import torch
+from torch.utils.data import DataLoader
+from torch.nn.functional import softmax
+from torch.distributions.categorical import Categorical
+from transformers import AutoTokenizer
+
+from utilities.data import MedicalDxDataset
+from utilities.model import BertDxModel
+from utilities.utils import move_bert_input_to_device
 
 class DiagnosisClassifier(object):
-    def __init__(self, model: BertDxModel):
+    def __init__(self, 
+            model: BertDxModel, 
+            tokenizer: AutoTokenizer,
+            id2dx: Dict[int, Any],
+            batch_size: int,
+            device: str,
+            verbose: bool = False
+        ):
         self.model = model
+        self.tokenizer = tokenizer
+        self.id2dx = {int(k): v for k, v in id2dx.items()} # make sure that ids are integers
+        self.dx2id = {v: k for k, v in self.id2dx.items()}
+        self.batch_size = batch_size
+        self.device = device
+        self.verbose = verbose
     
-    def predict(emrs: List[str]):
-        return
+    def predict(self, emrs: List[str]) -> torch.FloatTensor:
+        # make dataset and dataloader
+        dataset = MedicalDxDataset(emrs=emrs, dx_labels=[-100] * len(emrs), tokenizer=self.tokenizer)
+        dataloader = DataLoader(dataset, self.batch_size, shuffle=False, pin_memory=True, collate_fn=dataset.collate_fn)
+
+        all_logits = list()
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        for X, _ in tqdm(dataloader) if self.verbose else dataloader:
+            X = move_bert_input_to_device(X, device=self.device)
+            with torch.no_grad():
+                logits = self.model(X)
+                all_logits.append(logits)
+        
+        all_logits = torch.cat(all_logits, dim=0).cpu()
+        return all_logits
+    
+    def get_top_dxs_with_probs(self, logits: torch.FloatTensor, top_k: int) -> Tuple:
+        logits, preds = logits.sort(dim=-1, descending=True)
+        dxs = [list(map(lambda id_: self.id2dx[id_], pred)) for pred in preds[:, :top_k].tolist()]
+        probs = softmax(logits, dim=-1)[:, :top_k].tolist()
+        
+        return dxs, probs
+    
+    def calc_entropy(self, logits: torch.FloatTensor) -> torch.FloatTensor:
+        dist = Categorical(logits=logits)
+        entropies = dist.entropy()
+
+        return entropies
