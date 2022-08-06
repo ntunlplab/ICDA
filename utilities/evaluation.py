22785385320f95a56885fb3a7e840e7fd0d1f205
Brian-Ckwu <sonny40920710@gmail.com>
2022-08-06 19:57:29 +0800

    add evaluate function for finindg normalization

--- utilities/evaluation.py
+++ utilities/evaluation.py
@@ -4,6 +4,7 @@ import sklearn.metrics
 from colorama import Fore, Style
 from tqdm.auto import tqdm
 from typing import List, Tuple, Dict, Any
+from argparse import Namespace
 
 import seqeval.metrics
 from seqeval.scheme import IOB2
@@ -11,7 +12,7 @@ from seqeval.scheme import IOB2
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertModel, BertTokenizerFast, AutoTokenizer
 
-from .model import BertDxModel
+from .model import BertDxModel, BiEncoder
 from .utils import move_bert_input_to_device
 
 def predict_whole_set_ner(model: BertModel, data_loader: DataLoader, device: str) -> list:
@@ -151,6 +152,43 @@ def evaluate_dx_model(model: BertDxModel, eval_loader: DataLoader, device: str, 
         "hat8": sklearn.metrics.top_k_accuracy_score(y_true, logits, k=8, labels=range(eval_loader.dataset.num_dx_labels))
     }
 
+def evaluate_nen(data_loader: DataLoader, model: BiEncoder, args: Namespace, entity_embeddings: torch.FloatTensor = None, entities_loader: DataLoader = None) -> float:
+    assert entities_loader
+    # Load or construct entity embeddings
+    if entity_embeddings != None:
+        all_y_ents = entity_embeddings.to(args.device)
+    else:
+        all_y_ents = model.encode_all_entities(entities_loader, args).to(args.device)
+    
+    # Evaluation
+    all_cuis = entities_loader.dataset._ids
+    total_correct = 0
+    total_predict = 0
+
+    model.eval()
+    for emr_be, mention_indices_l, target_cuis, _ in data_loader: # No need of negative_cuis_l
+        emr_be = move_bert_input_to_device(emr_be, args.device)
+
+        with torch.no_grad():
+            # Encode mentions
+            y_ments = model.encode_mentions(emr_be, mention_indices_l)
+            assert len(y_ments) == len(mention_indices_l) == len(target_cuis)
+
+            # Calculate scores
+            scores = model.calc_scores(y_ments, all_y_ents)
+            
+            # Check correctness
+            preds = scores.argmax(dim=-1).cpu().tolist()
+            # Calculate num correct
+            for pred, target_cui in zip(preds, target_cuis):
+                pred_cui = all_cuis[pred]
+                if pred_cui == target_cui:
+                    total_correct += 1
+            
+            total_predict += len(preds)
+    
+    fullset_acc = total_correct / total_predict
+    return fullset_acc
 
 pollabel_color_mappings = {
     0: Style.RESET_ALL,
